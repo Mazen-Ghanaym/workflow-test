a32654cd05c9f492c51b2c01e4f5d463cc578f80
Mazen-Ghanaym <mznghanayem@gmail.com>
2025-10-16 02:29:27 +0300

    another update

--- src/test.sh
+++ src/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #src/test.sh
-EXPECTED="Hello, World!"
-OUTPUT=$(node -e "console.log(require('./src/app')('World'))")
+EXPECTED="Hello, Test!"
+OUTPUT=$(node -e "console.log(require('./src/app')('Test'))")
 if [ "$OUTPUT" == "$EXPECTED" ]; then
     echo "Test passed"
     exit 0
